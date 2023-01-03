26f965ccb88b76749f6863ece48160a1d9e11e81
Petr Ilin <hevav@hevav.dev>
2023-01-03 17:45:14 +0300

    Simpler way to block protocol versions
    Former-commit-id: 35bdfa4b9f9f82c6fd7edcf2c5719b1074c11f6d

--- src/main/java/net/elytrium/velocitytools/listeners/ProtocolBlockerPingListener.java
+++ src/main/java/net/elytrium/velocitytools/listeners/ProtocolBlockerPingListener.java
@@ -20,24 +20,35 @@ package net.elytrium.velocitytools.listeners;
 import com.velocitypowered.api.event.PostOrder;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.proxy.ProxyPingEvent;
+import com.velocitypowered.api.network.ProtocolVersion;
 import com.velocitypowered.api.proxy.server.ServerPing;
 import java.util.List;
+import java.util.stream.Collectors;
 import net.elytrium.velocitytools.Settings;
 import net.elytrium.velocitytools.VelocityTools;
+import net.elytrium.velocitytools.utils.ProtocolUtil;
 import net.elytrium.velocitytools.utils.WhitelistUtil;
 import net.kyori.adventure.text.Component;
 
 public class ProtocolBlockerPingListener {
 
   private final boolean whitelist;
-  private final List<Integer> protocols;
+  private final List<Integer> protocolNumbers;
+  private final List<ProtocolVersion> protocolVersions;
+  private final ProtocolVersion minimumProtocolVersion;
+  private final ProtocolVersion maximumProtocolVersion;
   private final String brand;
   private final String motd;
   private final Component motdComponent;
 
   public ProtocolBlockerPingListener() {
     this.whitelist = Settings.IMP.TOOLS.PROTOCOL_BLOCKER.WHITELIST;
-    this.protocols = Settings.IMP.TOOLS.PROTOCOL_BLOCKER.PROTOCOLS;
+    this.protocolNumbers = Settings.IMP.TOOLS.PROTOCOL_BLOCKER.PROTOCOLS;
+    this.protocolVersions = Settings.IMP.TOOLS.PROTOCOL_BLOCKER.VERSIONS.stream()
+        .map(ProtocolUtil::protocolVersionFromString)
+        .collect(Collectors.toList());
+    this.minimumProtocolVersion = ProtocolUtil.protocolVersionFromString(Settings.IMP.TOOLS.PROTOCOL_BLOCKER.MINIMUM_VERSION);
+    this.maximumProtocolVersion = ProtocolUtil.protocolVersionFromString(Settings.IMP.TOOLS.PROTOCOL_BLOCKER.MAXIMUM_VERSION);
     this.brand = Settings.IMP.TOOLS.PROTOCOL_BLOCKER.BRAND;
     this.motd = Settings.IMP.TOOLS.PROTOCOL_BLOCKER.MOTD;
     this.motdComponent = VelocityTools.getSerializer().deserialize(this.motd);
@@ -47,9 +58,13 @@ public class ProtocolBlockerPingListener {
   public void onPing(ProxyPingEvent event) {
     ServerPing.Builder builder = event.getPing().asBuilder();
 
-    int playerProtocol = event.getConnection().getProtocolVersion().getProtocol();
-    if (WhitelistUtil.checkForWhitelist(this.whitelist, this.protocols.contains(playerProtocol))) {
-      builder.version(new ServerPing.Version(playerProtocol + 1, this.brand));
+    ProtocolVersion playerProtocol = event.getConnection().getProtocolVersion();
+    int playerProtocolNumber = event.getConnection().getProtocolVersion().getProtocol();
+    if (this.minimumProtocolVersion.compareTo(playerProtocol) > 0 || this.maximumProtocolVersion.compareTo(playerProtocol) < 0
+        || WhitelistUtil.checkForWhitelist(this.whitelist,
+        this.protocolNumbers.contains(playerProtocolNumber)
+            || this.protocolVersions.contains(playerProtocol))) {
+      builder.version(new ServerPing.Version(playerProtocolNumber + 1, this.brand));
       if (!this.motd.isEmpty()) {
         builder.description(this.motdComponent);
       }
